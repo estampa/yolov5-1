6ecd7b172b726325570c34b226e301dcacd510e3
Estampa <56445942+estampa@users.noreply.github.com>
2022-05-10 16:10:34 +0200

    Amb l'avc1 fallava a colab

--- detect.py
+++ detect.py
@@ -249,7 +249,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
                         else:  # stream
                             fps, w, h = 30, im0.shape[1], im0.shape[0]
                         save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'avc1'), fps, (w, h))
+                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer[i].write(im0)
 
         # Print time (inference-only)
