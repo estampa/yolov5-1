402adadabd0d8033833768e6edc588ac9ec6dee4
Pau Artigas <estampa@tallerestampa.com>
2024-07-08 13:09:21 +0200

    Restored colors

--- utils/plots.py
+++ utils/plots.py
@@ -31,10 +31,9 @@ class Colors:
     # Ultralytics color palette https://ultralytics.com/
     def __init__(self):
         # hex = matplotlib.colors.TABLEAU_COLORS.values()
-        # hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
-        #        '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
-        hex = ('fcaa00', '0004ff', 'dafe01', 'ff0014', '1ffe00', '1ffe00', '00a9fe', '8efe00', '00fecf', 'fdf000',
-               'ff009e')
+        hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
+               '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
+        # hex = ('cccccc', 'cccccc')
         self.palette = [self.hex2rgb('#' + c) for c in hex]
         self.n = len(self.palette)
 
